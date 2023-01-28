45e6f45cdf70f58c8782d1477232b917643ed8b9
optimisme <optimisme@gmail.com>
2023-01-28 12:20:58 +0100

    Using fxml

---
+++ src/Controller0.java
@@ -0,0 +1,52 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+public class Controller0 implements Initializable {
+
+    @FXML
+    private AnchorPane anchor;
+
+    @FXML
+    private ChoiceBox<String> choiceBox = new ChoiceBox<>();
+    private String choiceBoxValues[] = { "Linies", "Poligons", "Poligons emplenats", 
+        "Quadrats i cercles", "Imatges", "Gradients lineals", "Gradients radials", 
+        "Transformacions", "Texts", "Text multilinia" };
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private Canvas canvas;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        // Initialize choiceBox values and action
+        choiceBox.getItems().addAll(choiceBoxValues);
+        choiceBox.setValue("Linies");
+        choiceBox.setOnAction((event) -> {
+            Object selectedItem = choiceBox.getSelectionModel().getSelectedItem();
+            Main.selectedDrawing = (String) selectedItem;
+        });
+
+        // Initialize canvas responsive size
+        UtilsViews.stage.heightProperty().addListener((observable, oldValue, newvalue) -> {
+            double size = UtilsViews.stage.getHeight() - hbox.getHeight();
+            canvas.setHeight(size);
+        });
+        UtilsViews.stage.widthProperty().addListener((observable, oldValue, newvalue) -> {
+            double size = UtilsViews.stage.getWidth();
+            canvas.setWidth(size);
+        });
+
+        // Start drawing loop
+        Main.drawing.start(canvas);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,15 @@
 import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
  
 public class Main extends Application {
 
-    private Drawing drawing = new Drawing();
+    public static Drawing drawing = new Drawing();
     public static ArrayList<DrawingObj> drawingList = new ArrayList<>();
 
     public static String selectedDrawing = "Linies";
@@ -33,95 +26,33 @@ public class Main extends Application {
     }
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws Exception {
 
-        int windowHeight = 500;
-        int windowWidth = 750;
+        final int windowWidth = 800;
+        final int windowHeight = 600;
 
-        // Construir interficie
-        VBox root = buildInterface(primaryStage);
+        UtilsViews.stage = stage;
+        UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
+        UtilsViews.addView(getClass(), "View0", "./assets/view0.fxml");
 
-        // Definir escena
-        Scene  scene = new Scene(root);
+        Scene scene = new Scene(UtilsViews.parentContainer);
         scene.addEventFilter(KeyEvent.ANY, keyEvent -> { keyEvent(keyEvent); });
+        
+        stage.setScene(scene);
+        stage.onCloseRequestProperty(); // Call close method when closing window
+        stage.setTitle("Exemples Canvas");
+        stage.setMinWidth(windowWidth);
+        stage.setMinHeight(windowHeight);
+        stage.show();
 
-        // Iniciar finestra d'app
-        primaryStage.setTitle("Exemples Canvas");
-        primaryStage.onCloseRequestProperty();
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        primaryStage.setResizable(true);
-        primaryStage.setHeight(windowHeight);
-        primaryStage.setWidth(windowWidth);
-        primaryStage.setMinHeight(windowHeight);
-        primaryStage.setMinWidth(windowWidth);
-        primaryStage.heightProperty().addListener((observable, oldValue, newvalue) -> {
-            double titleHeight = primaryStage.getHeight() - scene.getHeight();
-            double rootHeight = primaryStage.getHeight() - titleHeight;
-            root.setPrefHeight(rootHeight);
-        });
-
-        // Definir icona d'app
         Image icon = new Image("file:./assets/icon.png");
-        primaryStage.getIcons().add(icon);
+        stage.getIcons().add(icon);
     }
 
     @Override
     public void stop() {
         // Aturar el bucle de dibuix
         drawing.stop(); 
-    }
-
-    public VBox buildInterface(Stage primaryStage) {
-
-        // Definir la divisió vertical
-        VBox vbox = new VBox(0);
-        vbox.setAlignment(Pos.TOP_CENTER);
-
-            // Definir el selector superior
-            HBox hbox = new HBox(0);
-            hbox.setAlignment(Pos.CENTER);
-            
-                Label text = new Label("Dibuix: ");
-                text.setMaxWidth(Double.MAX_VALUE);
-                text.setAlignment(Pos.CENTER_RIGHT);
-
-                ComboBox<String> comboBox = new ComboBox<String>();
-                comboBox.getItems().add("Linies");
-                comboBox.getItems().add("Poligons");
-                comboBox.getItems().add("Poligons emplenats");
-                comboBox.getItems().add("Quadrats i cercles");
-                comboBox.getItems().add("Imatges");
-                comboBox.getItems().add("Gradients lineals");
-                comboBox.getItems().add("Gradients radials");
-                comboBox.getItems().add("Transformacions");
-                comboBox.getItems().add("Texts");
-                comboBox.getItems().add("Text multilinia");
-                comboBox.getSelectionModel().select(0);
-                comboBox.setOnAction((event) -> {
-                    Object selectedItem = comboBox.getSelectionModel().getSelectedItem();
-                    selectedDrawing = (String) selectedItem;
-                });
-
-                hbox.getChildren().addAll(text, comboBox);
-                HBox.setHgrow(text, Priority.ALWAYS);
-                HBox.setHgrow(comboBox, Priority.NEVER);
-
-            // Definir l'area de dibuix
-            Canvas canvas = new Canvas(100, 100);
-            drawing.start(canvas);
-            canvas.widthProperty().bind(vbox.widthProperty());
-            vbox.heightProperty().addListener((observable, oldValue, newvalue) -> {
-                double canvasHeight = vbox.getHeight() - hbox.getHeight();
-                canvas.setHeight(canvasHeight);
-            });
-
-            
-        vbox.getChildren().addAll(hbox, canvas);
-        VBox.setVgrow(hbox, Priority.NEVER);
-        VBox.setVgrow(canvas, Priority.ALWAYS);
-
-        return vbox;
     }
 
     public void keyEvent (KeyEvent evt) {
