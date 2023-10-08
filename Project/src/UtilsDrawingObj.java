24f826747bdc974e5e9b2c02f8fc9f243c323ced
optimisme <optimisme@gmail.com>
2023-10-08 11:33:43 +0200

    Maven

--- Project/src/UtilsDrawingObj.java
+++
@@ -1,8 +0,0 @@
-
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-
-interface UtilsDrawingObj {
-    public void run(Canvas cnv, double fps);
-    public void draw(GraphicsContext gc);
-}
