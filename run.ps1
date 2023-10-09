836c90be5e22f393fe832139b04e081539ad2cb0
optimisme <optimisme@gmail.com>
2023-10-09 11:35:22 +0200

    .

--- run.ps1
+++ run.ps1
@@ -5,12 +5,12 @@ Set-Location $PSScriptRoot
 
 # Search for JavaFX jars and generate the module path without specifying a specific version
 $FX_PATHS = Get-ChildItem -Path "$env:USERPROFILE\.m2\repository\org\openjfx" -Recurse -Filter "javafx-*.jar" | 
-            Where-Object { $_.Name -match "javafx-(controls|fxml)-.*\.jar" } | 
+            Where-Object { $_.Name -match "javafx-(base|controls|fxml|graphics).*\.jar" } | 
             ForEach-Object { $_.DirectoryName } | Sort-Object -Unique
 $FX_PATH = ($FX_PATHS -join ";")
 
 # Set MAVEN_OPTS environment variable
-$env:MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --module-path $FX_PATH --add-modules javafx.controls,javafx.fxml"
+$env:MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --module-path $FX_PATH --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics"
 
 # Check for the first argument and set it as the main class
 $mainClass = $args[0]
