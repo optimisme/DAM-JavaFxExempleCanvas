51cdf4deb4964af09ebedf4e8b1837c401b174ee
optimisme <optimisme@gmail.com>
2023-10-09 11:46:20 +0200

    .

--- run.ps1
+++ run.ps1
@@ -9,8 +9,14 @@ $FX_PATHS = Get-ChildItem -Path "$env:USERPROFILE\.m2\repository\org\openjfx" -R
             ForEach-Object { $_.DirectoryName } | Sort-Object -Unique
 $FX_PATH = ($FX_PATHS -join ";")
 
+# Get the Maven classpath
+$MVN_CLASSPATH = mvn dependency:build-classpath | Where-Object { $_ -match "Dependencies classpath:" } | % { $_ -replace "Dependencies classpath:", "" }
+
+# Combine the classpath and the module-path
+$FULL_PATH = "$MVN_CLASSPATH;$FX_PATH"
+
 # Set MAVEN_OPTS environment variable
-$env:MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --module-path $FX_PATH --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics"
+$env:MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --module-path $FULL_PATH --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics"
 
 # Check for the first argument and set it as the main class
 $mainClass = $args[0]
